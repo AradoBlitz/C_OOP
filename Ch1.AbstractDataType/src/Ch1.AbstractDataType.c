3f754e62a7932dd508b9e5ee9f1805db5ecb4772
vilidmi <vilidmi@vilidmi>
2017-04-16 12:26:58 +0300

    Opened chapter 0:1 Abstract Data Type - Information Hiding.

---
+++ Ch1.AbstractDataType/src/Ch1.AbstractDataType.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : AbstractDataType.c
+ Author      : Dmitry Vilyuzhain
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
